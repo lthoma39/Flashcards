84004ff46c409bbf0ebf90e3c217dfc71f19eeb0
Latrell Thomas <latrellthomas@latrells-macbook-pro.local>
2018-10-14 22:17:49 -0500

    Added the color green to indicate the answer.

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -26,6 +26,7 @@ class ViewController: UIViewController {
 
     @IBAction func didTapOnFlashcard(_ sender: Any) {
         FrontLabel.isHidden = true;
+        view.backgroundColor = UIColor.green
     }
 }
 
