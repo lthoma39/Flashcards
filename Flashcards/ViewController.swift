50638218b28f9f71077c6f1519f432a54180c266
latrellthomas <lthoma39@uic.edu>
2018-11-06 22:27:19 -0600

    Allows the user to switch between flashcards and their cards still exist after closing the app

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -9,6 +9,15 @@
 
 import UIKit
 
+struct Flashcard {
+    var question: String
+    var answer: String
+    var option1: String
+    var option2: String
+    var option3: String
+    var option4: String
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var FrontLabel: UILabel!
@@ -20,10 +29,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var btnOptionThree: UIButton!
     @IBOutlet weak var btnOptionFour: UIButton!
     
-    
     @IBOutlet weak var retButton: UIButton!
     
     var backgroundColor: UIColor!
+    
+    var flashcards = [Flashcard]()
+    var currentIndex = 0
+    
+    @IBOutlet weak var prevButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -59,6 +73,18 @@ class ViewController: UIViewController {
         
         retButton.isHidden = true
         
+        readSavedFlashcards()
+        
+        if (flashcards.count == 0){
+             updateFlashcard(question: "1 + 1", answer: "2", option1: "4", option2: "2", option3: "3", option4: "1")
+        }
+        else{
+            updateLabels()
+            updatePrevNextButtons()
+        }
+        
+       
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -71,24 +97,36 @@ class ViewController: UIViewController {
         //do nothing
     }
     
+    func updatePrevNextButtons(){
+        
+        if (currentIndex == flashcards.count - 1){
+            nextButton.isEnabled = false
+        }
+        else{
+            nextButton.isEnabled = true
+        }
+        
+        if(currentIndex == 0){
+            prevButton.isEnabled = false
+        }
+        else{
+            prevButton.isEnabled = true
+        }
+    }
+    
     func updateFlashcard(question: String, answer: String, option1: String?, option2: String?, option3: String?, option4: String?){
         
-        FrontLabel.text = question
-        BackLabel.text = answer
+        let flashcard = Flashcard(question: question, answer: answer, option1: option1!, option2: option2!, option3: option3!, option4: option4!)
         
-        if (!(option1?.isEmpty)!){
-           btnOptionOne.setTitle(option1, for: .normal)
-        }
-        if (!(option2?.isEmpty)!){
-            btnOptionTwo.setTitle(option2, for: .normal)
-        }
-        if (!(option3?.isEmpty)!){
-            btnOptionThree.setTitle(option3, for: .normal)
-        }
-        if (!(option4?.isEmpty)!){
-            btnOptionFour.setTitle(option4, for: .normal)
-        }
+        flashcards.append(flashcard)
+        print("Added a new flashcard")
+        print("We now have \(flashcards.count) flashcards")
         
+        currentIndex = flashcards.count - 1
+        print("Our current index is \(currentIndex) ")
+        
+        updatePrevNextButtons()
+        updateLabels()
     }
     
     @IBAction func didTapOptionOne(_ sender: Any){
@@ -105,6 +143,7 @@ class ViewController: UIViewController {
         card.backgroundColor = UIColor.cyan
         BackLabel.backgroundColor = UIColor.cyan
         retButton.isHidden = false
+        prevButton.isHidden = true
     }
     
     
@@ -128,9 +167,23 @@ class ViewController: UIViewController {
             btnOptionFour.isHidden = false
             card.backgroundColor = backgroundColor
             retButton.isHidden = true
+            prevButton.isHidden = false
         }
     }
     
+    @IBAction func didTapPrev(_ sender: Any){
+        currentIndex -= 1
+        updatePrevNextButtons()
+        updateLabels()
+        
+    }
+    
+    @IBAction func didTapNext(_ sender: Any){
+        
+        currentIndex += 1
+        updatePrevNextButtons()
+        updateLabels()
+    }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?){
         
@@ -144,6 +197,49 @@ class ViewController: UIViewController {
             creationController.initialQuestion = FrontLabel.text
             creationController.initialAnswer = BackLabel.text
         }
+    }
+    
+    func updateLabels(){
+        
+        let currentFlashcard = flashcards[currentIndex]
+        FrontLabel.text = currentFlashcard.question
+        BackLabel.text = currentFlashcard.answer
+        if (!(currentFlashcard.option1.isEmpty)){
+            btnOptionOne.setTitle(currentFlashcard.option1, for: .normal)
+        }
+        if (!(currentFlashcard.option2.isEmpty)){
+            btnOptionTwo.setTitle(currentFlashcard.option2, for: .normal)
+        }
+        if (!(currentFlashcard.option3.isEmpty)){
+            btnOptionThree.setTitle(currentFlashcard.option3, for: .normal)
+        }
+        if (!(currentFlashcard.option4.isEmpty)){
+            btnOptionFour.setTitle(currentFlashcard.option4, for: .normal)
+        }
+        
+        saveAllFlashcardsToDisk()
+    }
+    
+    func readSavedFlashcards(){
+        if let dictionaryArray = UserDefaults.standard.array(forKey: "flashcards") as?  [[String: String]]{
+            
+            let savedCards = dictionaryArray.map { dictionary -> Flashcard in
+                return Flashcard(question: dictionary["question"]!, answer: dictionary["answer"]!, option1: dictionary["option1"]!, option2: dictionary["option2"]!, option3: dictionary["option3"]!, option4: dictionary["option4"]!)
+            }
+            
+            flashcards.append(contentsOf: savedCards)
+        }
+    }
+    
+    func saveAllFlashcardsToDisk(){
+        
+        let dictionaryArray = flashcards.map { (card) -> [String: String] in
+            return ["question": card.question, "answer": card.answer, "option1": card.option1, "option2": card.option2, "option3": card.option3,
+                    "option4": card.option4]
+        }
+        
+        UserDefaults.standard.set(dictionaryArray, forKey: "flashcards")
+        
     }
 }
 
